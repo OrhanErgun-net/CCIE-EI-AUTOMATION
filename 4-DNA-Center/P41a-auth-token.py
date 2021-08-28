4b78563dd4faed95bd86b1c79c0a26d6b13aed89
Omar Adil <40440628+omarthe95@users.noreply.github.com>
2021-08-28 16:40:23 +0300

    Update P41a-auth-token.py

--- 4-DNA-Center/P41a-auth-token.py
+++ 4-DNA-Center/P41a-auth-token.py
@@ -1,1 +1,23 @@
+import urllib3, requests, json, sys
 
+# Disable SSL Warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Auth Creds
+#Auth=(UserName,PassWord)
+Auth=("admin","P@ssw0rd")
+
+# Headers
+Headers={'Accept': 'application/json',
+        'Content-Type': 'application/json'}
+
+# Main URL
+DNAC = "https://192.168.222.111"
+
+# URL for auth login and token
+URL_Login = "/dna/system/api/v1/auth/token"
+
+# Login using Post
+Res_Login = requests.post(url=f"{DNAC}{URL_Login}", auth=Auth, headers=Headers, verify=False)
+
+print (Res_Login.text)
